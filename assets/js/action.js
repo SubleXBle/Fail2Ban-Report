2addf71cd6ae5462f4b77e1f4df086fb0df5b594
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-07-28 04:15:09 +0200

    Update action.js

--- assets/js/action.js
+++ assets/js/action.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   tbody.addEventListener('click', e => {
     if (e.target.classList.contains('action-btn')) {
       const ip = e.target.dataset.ip;
-      collectAndExecuteActions(ip); // <- central Logic in this File
+      const jail = e.target.dataset.jail || '';
+      collectAndExecuteActions(ip, jail);
     }
   });
 });
