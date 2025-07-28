135a3d27e6afda21329f70f833b3c0425ba3c17f
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-07-28 02:53:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     const availableFiles = <?php echo $filesJson; ?>;
   </script>
   <script src="/assets/js/jsonreader.js" defer></script>
+  <script src="/js/action-collector.js"></script>
   <script src="/assets/js/actions.js" defer></script>
 </head>
 <body>
