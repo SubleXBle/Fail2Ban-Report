8c92a7fd8d5a03b3ccbdc0206c1cfb969a9c8cc8
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-08-06 17:32:58 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="site-footer">
   <div class="footer-content">
-    &copy; <?php echo date('Y'); ?> SubleXBle (<a href="https://github.com/SubleXBle" title="SubleXBle on Github" target="_blank">https://github.com/SubleXBle</a>) All rights reserved.
+    <span title="Made with sweat, tears and love by SubleXBle">Made with 🥵, 😿 and ❤️ </span>by <a href="https://github.com/SubleXBle" title="SubleXBle on Github" target="_blank" class="yellowtext">SubleXBle</a> &copy; <?php echo date('Y'); ?> All rights reserved
   </div>
 </footer>
